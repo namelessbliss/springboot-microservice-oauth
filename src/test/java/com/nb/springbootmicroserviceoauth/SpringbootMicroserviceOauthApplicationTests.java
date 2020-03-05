a951f5deec4b416e8ed8795e2ec60f86b87c8358
nb <anonimo2911.2012@gmail.com>
2020-03-05 16:03:37 -0500

    proyecto inicial + dependencia usuario commons + properties

---
+++ src/test/java/com/nb/springbootmicroserviceoauth/SpringbootMicroserviceOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.springbootmicroserviceoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMicroserviceOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
